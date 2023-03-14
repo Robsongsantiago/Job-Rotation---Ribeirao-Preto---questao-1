d53c6619d9ba24668696adb266c0c2cc17535816
Robsongsantiago <robsongsantiago2525@gmail.com>
2023-03-14 19:18:04 -0300

    Adicionar arquivos de projeto.

---
+++ Job Rotation - Ribeirão Preto - Questão 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Rotation___Ribeirão_Preto___Questão_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 13;
+
+            int SOMA = 0;
+
+            int K = 0;
+
+            while (K < INDICE)
+            {
+                K = K + 1;
+
+                SOMA = SOMA + K;
+            }
+
+            Console.WriteLine(SOMA);
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
